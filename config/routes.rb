6366bb70981dc48a7aae329ba2cb7747c57718c4
Jake Oldhamhu33er <jakemoldham@gmail.com>
2015-06-22 17:01:11 -0600

    added staticpages home as root, also created and migrated the pg database

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
 
-  get 'static_pages/about'
+  get  'static_pages/about'
 
-  get 'static_pages/contact'
+  get  'static_pages/contact'
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
